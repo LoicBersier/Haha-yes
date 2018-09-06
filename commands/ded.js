76e6d3f611d238666514e2de4a352f22b3e3892e
Loïc Bersier <loic.bersier1@gmail.com>
2018-09-06 17:28:43 +0200

    shut down the bot but dosent send the message to shut it down idk why

--- commands/ded.js
+++ commands/ded.js
@@ -1,1 +1,6 @@
-process.exit();
+exports.run = (client,message) => {
+    if (message.author.id == "267065637183029248") {
+        message.channel.send("haha yes im ded now k bye thx").catch(console.error);
+        process.exit();
+}
+};
