b1c5e96e846d85b514e03491f529ba8927e5b2a3
Loïc Bersier <loic.bersier1@gmail.com>
2018-09-06 18:59:55 +0200

    re added prefix

---
+++ commands/prefix.js
@@ -0,0 +1,10 @@
+exports.run = (client, message, args) => {
+    if (message.author.id == "267065637183029248") {
+    // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
+    let newPrefix = message.content.split(" ").slice(1, 2)[0];
+    // change the configuration in memory
+    config.prefix = newPrefix;
+    // Now we have to save the file.
+    fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
+  }
+}
