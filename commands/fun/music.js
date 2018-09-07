889cef0f8f1469eae0a0729cc2200532731460fa
Loïc Bersier <loic.bersier1@gmail.com>
2018-09-08 00:22:32 +0200

    Music now work, bot can leave the channel

--- commands/fun/music.js
+++ commands/fun/music.js
@@ -10,7 +10,7 @@ module.exports = class MusicCommand extends Command {
             args: [
                 {
                     key: 'ytblink',
-                    prompt: 'Wich Youtube link would you like to play?',
+                    prompt: 'Wich Youtube link would you like to play? (stop to make the bot leave the channel)',
                     type: 'string',
                 }
             ]
@@ -24,6 +24,9 @@ module.exports = class MusicCommand extends Command {
             return message.reply('please join a voice channel first!');
         }
 
+        if (ytblink == 'stop') {
+            voiceChannel.leave()
+        } else 
         voiceChannel.join().then(connection => {
             const stream = ytdl(ytblink, { filter: 'audioonly' });
             const dispatcher = connection.playStream(stream);
