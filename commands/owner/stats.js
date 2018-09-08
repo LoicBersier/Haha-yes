fdd22425f95aa03b4e483267da414c60f77f003b
Loïc Bersier <loic.bersier1@gmail.com>
2018-09-08 16:10:28 +0200

    Show stats of bot

---
+++ commands/owner/stats.js
@@ -0,0 +1,15 @@
+const { Command } = require('discord.js-commando');
+module.exports = class MeowCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'stats',
+            group: 'owner',
+            memberName: 'stats',
+            description: 'Show bot stats.',
+        });
+    }
+
+    run(message) {
+        return message.channel.send(`Servers: ${this.client.guilds.size}\nChannels: ${this.client.channels.size}\nUsers : ${this.client.users.size}`);
+    }
+};
