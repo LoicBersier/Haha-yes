53f9f345397e64a6647913e03070f64b417ca2b6
Loïc Bersier <loic.bersier1@gmail.com>
2018-09-08 20:38:39 +0200

    added vid

--- commands/fun/despacito.js
+++ commands/fun/despacito.js
@@ -11,7 +11,7 @@ module.exports = class DespacitoCommand extends Command {
     }
 
     run(message) {
-        const number = 2;
+        const number = 4;
         const vidNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
             message.channel.send(responseObject[vidNumber]);
           }
