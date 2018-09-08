64816b2f0dd09cd4e8962aa0f2d87dc1d10cf999
Loïc Bersier <loic.bersier1@gmail.com>
2018-09-09 00:44:35 +0200

    Vote for the bot !

---
+++ commands/thing/updoot.js
@@ -0,0 +1,28 @@
+const { Command } = require('discord.js-commando');
+module.exports = class UpDootCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'updoot',
+            aliases: 'vote',
+            group: 'thing',
+            memberName: 'updoot',
+            description: 'Send link to updoot my bot :D.',
+        });
+    }
+
+    run(message) {
+        const upDoot = {
+            color: 0x93C54B,
+            title: 'Vote for my bot',
+            url: 'https://discordbots.org/bot/377563711927484418/vote',
+            description: 'You can vote for my bot if you think the bot is awesome!',
+            timestamp: new Date(),
+            footer: {
+                text: 'Thanks for the updoots',
+                icon_url: 'https://cdn.discordapp.com/avatars/377563711927484418/1335d202aa466dbeaa4ed2e4b616484a.png?size=2048',
+            },
+        };
+        
+        message.channel.send({ embed: upDoot });
+    }
+};
