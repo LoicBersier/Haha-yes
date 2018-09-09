60d6c6bd2c5a66aacf8b5864509f0bdba367e3df
Loïc Bersier <loic.bersier1@gmail.com>
2018-09-09 20:07:28 +0200

    updated fun fact description and the way it pick them

--- commands/fun/funfact.js
+++ commands/fun/funfact.js
@@ -6,12 +6,12 @@ module.exports = class FunFactCommand extends Command {
             name: 'funfact',
             group: 'fun',
             memberName: 'funfact',
-            description: 'Replies with a some fun fact.',
+            description: `Send some fun fact. If you would like to see some of yours fun fact you can send them to @Supositware | Baguette#8211. There is currently **${Object.keys(responseObject).length}** fun fact.`,
         });
     }
 
     run(message) {
-        const number = 2;
+        const number = Object.keys(responseObject).length;
         const funFactNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
             message.channel.send(`Fun fact: ${responseObject[funFactNumber]}`);
     }
