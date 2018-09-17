86a7c0f4f03aa9d3ac27de927a677409c3786b5b
Loic Bersier <loic.bersier1@gmail.com>
2018-09-17 22:07:32 +0200

    Added back auto reply

--- index.js
+++ index.js
@@ -64,11 +64,11 @@ client.registry
     })
 
 //  Auto respond to messages
-//    client.on("message", (message) => {
-//        if(responseObject[message.content]) {
-//          message.channel.send(responseObject[message.content]);
-//        }
-//      });
+    client.on("message", (message) => {
+        if(responseObject[message.content]) {
+          message.channel.send(responseObject[message.content]);
+        }
+      });
 
     client.on('error', console.error);
 
