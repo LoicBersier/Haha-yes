75f30c467e13af860e4c0a51519ab5318e15981a
Loic Bersier <loic.bersier1@gmail.com>
2018-09-19 16:44:00 +0200

    Changed the permissions on the invite link

--- commands/utility/invite.js
+++ commands/utility/invite.js
@@ -10,6 +10,6 @@ module.exports = class InviteCommand extends Command {
     }
 
     async run(message) {
-        return message.say(`You can add me from here: https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&scope=bot&permissions=2054\nYou can join my support server over here: https://discord.gg/SsMCsVY come and say hi :)`);
+        return message.say(`You can add me from here: https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&scope=bot&permissions=27654\nYou can join my support server over here: https://discord.gg/SsMCsVY come and say hi :)`);
     }
 };
