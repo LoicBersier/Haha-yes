7eff6dd931daee23084aa4fa5b8efa49847bb114
Loic Bersier <loic.bersier1@gmail.com>
2018-09-20 20:47:17 +0200

    Chatbleach ( just spam a bunch of dot to "clean" a channel )

---
+++ commands/admin/cb.js
@@ -0,0 +1,19 @@
+const { Command } = require('discord.js-commando');
+module.exports = class cbCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'cb',
+            group: 'admin',
+            memberName: 'cb',
+            description: 'spam a bunch of dot to quickly make something disspear',
+            clientPermissions: ['MANAGE_MESSAGES'],
+            userPermissions: ['MANAGE_MESSAGES'],
+            guildOnly: true,
+        });
+    }
+
+    run(message) {
+       message.say('.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.')
+    }
+};
+
