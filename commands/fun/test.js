2464a0f75c1b4866d4de3724371b805ac62b1ca1
Loic Bersier <loic.bersier1@gmail.com>
2018-09-22 18:27:06 +0200

    Repeat the text in dancing letters ( probably bad code have to make it better )

---
+++ commands/fun/test.js
@@ -0,0 +1,33 @@
+const { Command } = require('discord.js-commando');
+const emojiCharacters = require('../../emojiCharacters');
+module.exports = class testCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'emotesay',
+            group: 'fun',
+            memberName: 'emotesay',
+            description: `repeat the text in dancing letters`,
+            args: [
+                {
+                    key: 'text',
+                    prompt: 'What do you want me to say',
+                    type: 'string',
+                }
+            ]
+        });
+    }
+
+    async run(message, { text }) {
+        let textChar = text.split('')
+        let i = text.length;
+        let emojiArray = [];
+        message.delete();
+        for (i = 0; i < text.length; i++) {
+            emojiArray[i] = emojiCharacters[textChar[i]];
+        }
+        let finalText = emojiArray.join("");
+        message.say(finalText)
+        
+
+    }
+};
