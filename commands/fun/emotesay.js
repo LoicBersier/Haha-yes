30c004111a06886cb39695dc4295630f8d3188ee
John Doe <loic.bersier1@gmail.com>
2018-09-23 20:04:16 +0200

    fixed a mistake

--- commands/fun/emotesay.js
+++ commands/fun/emotesay.js
@@ -22,7 +22,7 @@ module.exports = class emoteSayCommand extends Command {
         text.toLowerCase().split('')
         let emojiArray = [];
         for (let i = 0; i < text.length; i++) {
-            emojiArray[i] = emojiCharacters[textChar[i]];
+            emojiArray[i] = emojiCharacters[text[i]];
         }
         message.say(emojiArray.join(""))
         
