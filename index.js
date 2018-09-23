e494e0d82ca98c1132fb4aa6581f565423ec5764
unknown <loic.bersier1@gmail.com>
2018-09-23 23:12:32 +0200

    Fixed something i removed by accident

--- index.js
+++ index.js
@@ -31,9 +31,11 @@ client.registry
         console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
         console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users. ${client.readyAt}`);
 //  Send stats to the "stats" channel in the support server if its not the test bot
+        if (client.user.id === botID) {
         const channel = client.channels.get(statsChannel);
         channel.send(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users. ${client.readyAt}`);
         client.user.setActivity('"haha help" or "@me help" for help');
+    }
 });
 //  When bot join a guild send embeds with details about it.
     client.on("guildCreate", async guild => {
