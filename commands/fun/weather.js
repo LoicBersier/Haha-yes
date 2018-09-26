7b3bedc08ca17bfa8cc76d3c235e5bf75986a258
loicbersier <loic.bersier1@gmail.com>
2018-09-26 13:35:54 +0200

    renamed the "test" embed

--- commands/fun/weather.js
+++ commands/fun/weather.js
@@ -1,7 +1,7 @@
 const { Command } = require('discord.js-commando');
 const Discord = require('discord.js');
 const snekfetch = require('snekfetch');
-const { openweatherAPI } = require('./config.json');
+const { openweatherAPI } = require('../../config.json');
 module.exports = class WeatherCommand extends Command {
     constructor(client) {
         super(client, {
@@ -24,7 +24,7 @@ module.exports = class WeatherCommand extends Command {
         if (!body.main.temp) {
             return message.say(`No results found for **${city}**`);
         }
-        const test = new Discord.RichEmbed()
+        const weatherEmbed = new Discord.RichEmbed()
         .setColor("#ff9900")
         .setTitle(body.name + ' current weather')
         .setDescription(body.main.temp + '°c')
@@ -32,6 +32,6 @@ module.exports = class WeatherCommand extends Command {
         .addField('Temp max', body.main.temp_max + '°c')
 
 
-            message.say(test);
+            message.say(weatherEmbed);
           }
 };
