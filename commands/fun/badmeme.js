4734b5cf4d5ffbe1b4bf46433c820d04d526c207
loicbersier <loic.bersier1@gmail.com>
2018-10-02 10:43:17 +0200

    Take images from imgur

---
+++ commands/fun/badmeme.js
@@ -0,0 +1,29 @@
+const { Command } = require('discord.js-commando');
+const Discord = require('discord.js');
+const fetch = require('node-fetch')
+module.exports = class BadMemeCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'badmeme',
+            group: 'fun',
+            memberName: 'badmeme',
+            description: `Show a random dog`,
+        });
+    }
+
+    async run(message) {
+
+        fetch("https://api.imgur.com/3/gallery/hot/top/day?showViral=true&mature=false&perPage=100&album_previews=true", {
+            headers: { "Authorization": "Client-ID e4cb6948f80f295" },
+        }).then((response) => {
+  return response.json();
+}).then((response) => {
+    if (response.success == 'false')
+        return message.say('An error has occured')
+
+        const i = Math.floor((Math.random() * response.data.length));
+
+        message.say(`**${response.data[i].title}**`)
+        message.say(response.data[i].link); 
+          });
+}};
