abe405536ada3cb26cde698aff4f3ce50c68aa41
Loic Bersier <loic.bersier1@gmail.com>
2018-10-08 17:42:56 +0200

    Added a donation command ( will probably remove later )

---
+++ commands/utility/supportme.js
@@ -0,0 +1,15 @@
+const { Command } = require('discord.js-commando');
+module.exports = class supportMeCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'supportme',
+            group: 'fun',
+            memberName: 'supportme',
+            description: `Support me and my bot`,
+        });
+    }
+
+    async run(message, { text }) {
+            message.say('If you want to support me and my bot you can donate here\nhttps://www.paypal.me/supositware or here https://donatebot.io/checkout/487640086859743232\n(This is totally optionnal dont feel forced to do it)');
+          }
+};
