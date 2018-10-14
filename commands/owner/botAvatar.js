0e9120bf498fff45464fe858c54663871195fcad
loicbersier <loic.bersier1@gmail.com>
2018-10-14 17:38:45 +0200

    use attachement

--- commands/owner/botAvatar.js
+++ commands/owner/botAvatar.js
@@ -12,13 +12,20 @@ module.exports = class BotavatarCommand extends Command {
                     key: 'pic',
                     prompt: 'Wich avatar should i have?',
                     type: 'string',
+                    default: ''
                 }
             ]
         });
     }
 
     async run(message, { pic }) {
-        this.client.user.setAvatar(pic);
+        let Attachment = (message.attachments).array();
+        let image = null
+        if (!Attachment[0])
+            return message.say('You didint provide any images')
+        else 
+            image = Attachment[0].url
+        this.client.user.setAvatar(image);
         message.say('The avatar have been changed succesfully');
     }
 };
