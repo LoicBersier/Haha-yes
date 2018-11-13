fd7bead5aa5c7cedd9604a93d0c50fc8c7501cdd
Loïc Bersier <loic@loics-macbook-pro.home>
2018-11-13 15:20:03 +0100

    Can now mention user too

--- commands/images/idubbbzpaint.js
+++ commands/images/idubbbzpaint.js
@@ -20,20 +20,28 @@ module.exports = class idubbbzpaintCommand extends Command {
                     prompt: 'What do you the paper to say?',
                     type: 'string',
                     default: 'Perfection'
+                },
+                {
+                    key: 'user',
+                    prompt: 'What do you want me to say',
+                    type: 'user',
+                    default: ''
                 }
             ]
         });
     }
 
-    async run(message, { text }) {
+    async run(message, { text, user }) {
         if(blacklist[message.author.id])
         return message.channel.send("You are blacklisted")
         let Attachment = (message.attachments).array();
         let image = null
-        if (!Attachment[0])
-            image = message.author.displayAvatarURL
-        else if(Attachment[0] && Attachment[0].url.endsWith('gif'))
-            return message.say('Gif dosent work, sorry')
+        if (!Attachment[0] && !user)
+            image = message.author.displayAvatarURL;
+        else if (!Attachment[0])
+            image = user.displayAvatarURL;
+        else if(Attachment[0].url.endsWith('gif'))
+            return message.say('Gif dosent work, sorry');
         else 
         image = Attachment[0].url
 
