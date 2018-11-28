06af74b2cf8bdbed3821ce5662793e9453b0b60f
Loic Bersier <loic.bersier1@gmail.com>
2018-11-28 19:19:07 +0100

    reverted changes beacause it dosent change anything

--- commands/fun/random.js
+++ commands/fun/random.js
@@ -41,8 +41,8 @@ module.exports = class RandomCommand extends Command {
         text = text.replace(/\[adverb\]/, adverb[randNumber(adverb)])
         text = text.replace(/\[noun\]/, noun[randNumber(noun)])
         text = text.replace(/\[adjective\]/, adjective[randNumber(adjective)])
-        text = text.replace(/\[member\]/, message.guild.members.filter(member => !member.user.bot).random().user.username)
-        } while( text.includes('[verb]') || text.includes('[adverb]') || text.includes('[noun]') || text.includes('[adjective]' || text.includes('[member]')) )
+        text = text.replace(/\[member\]/, message.guild.members.random().user.username)
+        } while( text.includes('[verb]') || text.includes('[adverb]') || text.includes('[noun]') || text.includes('[adjective]' || text.includes('[member]' || text-includes('[adjectives]'))) )
 
 //      Send the final text
         message.say(text);
