852865e9a2c403188fca8c8eacb3fe37d3356c7b
Loic Bersier <loic.bersier1@gmail.com>
2018-11-28 18:25:57 +0100

    added patreons link

--- commands/utility/supportme.js
+++ commands/utility/supportme.js
@@ -16,6 +16,6 @@ module.exports = class supportMeCommand extends Command {
         if(blacklistJson[message.author.id])
         return blacklist(blacklistJson[message.author.id] , message)
         
-            message.say('If you want to support me and my bot you can donate here\nhttps://donatebot.io/checkout/487640086859743232\n(This is totally optional dont feel forced to do it)');
+            message.say('If you want to support me and my bot you can donate here\nhttps://donatebot.io/checkout/487640086859743232\nor here on patreon: https://patreon.com/user?u=15330358&utm_medium=social&utm_source=twitter&utm_campaign=creatorshare\n(This is totally optional dont feel forced to do it)');
           }
 };
