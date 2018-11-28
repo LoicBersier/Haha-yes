e7703d83d033d8893a92fd0a7b5a3e7b99920c26
Loic Bersier <loic.bersier1@gmail.com>
2018-11-28 20:27:29 +0100

    merged random with say

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -5,9 +5,10 @@ module.exports = class sayCommand extends Command {
     constructor(client) {
         super(client, {
             name: 'say',
+            aliases: ['repeat'],
             group: 'fun',
             memberName: 'say',
-            description: `Repeat the text you send`,
+            description: `Repeat the text you send ( can also use [verb] [noun] [adverb] [adjective] [member] and [number] to get random thing )`,
             args: [
                 {
                     key: 'text',
@@ -22,8 +23,29 @@ module.exports = class sayCommand extends Command {
         let blacklistJson = new SelfReloadJSON('json/blacklist.json');
         if(blacklistJson[message.author.id])
         return blacklist(blacklistJson[message.author.id] , message)
-        
-            message.delete();
-            message.say(text);
+
+
+//      Load all the different files
+        const verb = require('../../dictionary/verbs.json')
+        const noun = require('../../dictionary/nouns.json')
+        const adverb = require('../../dictionary/adjectives.json')
+        const adjective = require('../../dictionary/adverbs.json')
+//      Generate a random number
+        function randNumber(file) {
+            let Rand = Math.floor((Math.random() * file.length) + 1);
+            return Rand;
+        }
+//      Replace with a random word from the json
+        do {
+        text = text.replace(/\[verb\]/, verb[randNumber(verb)])
+        text = text.replace(/\[adverb\]/, adverb[randNumber(adverb)])
+        text = text.replace(/\[noun\]/, noun[randNumber(noun)])
+        text = text.replace(/\[adjective\]/, adjective[randNumber(adjective)])
+        text = text.replace(/\[member\]/, message.guild.members.random().user.username)
+        text = text.replace(/\[number\]/, Math.floor((Math.random() * 10) + 1))
+        } while( text.includes('[verb]') || text.includes('[adverb]') || text.includes('[noun]') || text.includes('[adjective]') || text.includes('[member]' || text.includes('[number]')) )
+
+//      Send the final text
+        message.say(text);
           }
 };
