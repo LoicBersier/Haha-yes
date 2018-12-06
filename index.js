a96bd9eb1343a1bebe245d2b1fd35f623a884abf
Loic Bersier <loic.bersier1@gmail.com>
2018-12-06 22:42:13 +0100

    rearrenged the autoresponse section and added user tag without relying on autoresponse

--- index.js
+++ index.js
@@ -75,45 +75,36 @@ client.registry
     });
 
     client.on("message", async (message) => {
-        var customresponse = new SelfReloadJSON(`DiscordBot/tag/${message.guild.id}.json`);
+        var customresponse = new SelfReloadJSON(`./tag/${message.guild.id}.json`);
+        var autoresponse = new SelfReloadJSON('./json/autoresponse.json');
 
         let message_content = message.content.toLowerCase();
-//  React to the message and send an auto response with it
         if (message.author.bot) return; {
-//  Reply with images as attachement
-        if(imgResponseObject[message_content]) {
-            var autoresponse = new SelfReloadJSON('DiscordBot/json/autoresponse.json');
-            if(autoresponse[message.channel.id] == 'enable')
-            message.channel.send({files: [imgResponseObject[message_content]]}); 
-        } 
-//  React only to the messages
-        else if(reactObject[message_content]) {
-            var autoresponse = new SelfReloadJSON('DiscordBot/json/autoresponse.json');
-            if(autoresponse[message.channel.id] == 'enable')
-            message.react(reactObject[message_content]);
-        }
-//  auto respond to messages
-        else if(responseObject[message_content]) {
-            var autoresponse = new SelfReloadJSON('DiscordBot/json/autoresponse.json');
-            if(autoresponse[message.channel.id] == 'enable')
-            message.channel.send(responseObject[message_content]);
-//  User autoresponse
-        } else if(customresponse[message_content]) {
-            var autoresponse = new SelfReloadJSON('DiscordBot/json/autoresponse.json');
+//  If autoresponse is enable send the response
             if(autoresponse[message.channel.id] == 'enable') {
-                message.channel.send(customresponse[message_content])
-            }
+//  Reply with images as attachement
+                if(imgResponseObject[message_content]) {
+                    message.channel.send({files: [imgResponseObject[message_content]]}); 
+                } 
+//  React only to the messages
+                else if(reactObject[message_content]) {
+                    message.react(reactObject[message_content]);
+                }
+//  auto respond to messages
+                else if(responseObject[message_content]) {
+                    message.channel.send(responseObject[message_content]);
 //  If it contain "like if" react with 👍
-        } else if (message_content.includes("like if")) {
-            var autoresponse = new SelfReloadJSON('DiscordBot/json/autoresponse.json');
-            if(autoresponse[message.channel.id] == 'enable')
-            message.react("\u{1F44D}")
+                } else if (message_content.includes("like if")) {
+                    message.react("\u{1F44D}")
 //  If it contain "jeff" react with a jeff emote
-        } else if (message_content.includes("jeff")) {
-            var autoresponse = new SelfReloadJSON('DiscordBot/json/autoresponse.json');
-            if(autoresponse[message.channel.id] == 'enable')
-            message.react("496028845967802378")
-        }
+                } else if (message_content.includes("jeff")) {
+                    message.react("496028845967802378")
+                }
+            }
+
+//  User autoresponse
+        if(customresponse[message_content])
+            message.channel.send(customresponse[message_content])
     }});
 
 //  Very basic starboard
