68cfa021bbd4f309411c421a7cf1ae72f86a73eb
Loic Bersier <loic.bersier1@gmail.com>
2018-12-08 05:13:05 +0100

    dosent throw a bunch of error

--- index.js
+++ index.js
@@ -75,38 +75,43 @@ client.registry
     });
 
     client.on("message", async (message) => {
-        var customresponse = new SelfReloadJSON(`./tag/${message.guild.id}.json`);
-        var autoresponse = new SelfReloadJSON('./json/autoresponse.json');
+        try {
+            var customresponse = new SelfReloadJSON(`./tag/${message.guild.id}.json`)
+            var autoresponse = new SelfReloadJSON('./json/autoresponse.json');
 
-        let message_content = message.content.toLowerCase();
-        if (message.author.bot) return; {
-
-//  User autoresponse
-            if(customresponse[message_content])
-                message.channel.send(customresponse[message_content])
-
-//  If autoresponse is enable send the response
-            if(autoresponse[message.channel.id] == 'enable') {
-//  Reply with images as attachement
-                if(imgResponseObject[message_content]) {
-                    message.channel.send({files: [imgResponseObject[message_content]]}); 
-                } 
-//  React only to the messages
-                else if(reactObject[message_content]) {
-                    message.react(reactObject[message_content]);
-                }
-//  auto respond to messages
-                else if(responseObject[message_content]) {
-                    message.channel.send(responseObject[message_content]);
-//  If it contain "like if" react with 👍
-                } else if (message_content.includes("like if")) {
-                    message.react("\u{1F44D}")
-//  If it contain "jeff" react with a jeff emote
-                } else if (message_content.includes("jeff")) {
-                    message.react("496028845967802378")
-                }
-            }
-    }});
+            let message_content = message.content.toLowerCase();
+            if (message.author.bot) return; {
+    
+    //  User autoresponse
+                if(customresponse[message_content])
+                    message.channel.send(customresponse[message_content])
+                    
+    
+    //  If autoresponse is enable send the response
+                if(autoresponse[message.channel.id] == 'enable') {
+    //  Reply with images as attachement
+                    if(imgResponseObject[message_content]) {
+                        message.channel.send({files: [imgResponseObject[message_content]]}); 
+                    } 
+    //  React only to the messages
+                    else if(reactObject[message_content]) {
+                        message.react(reactObject[message_content]);
+                    }
+    //  auto respond to messages
+                    else if(responseObject[message_content]) {
+                        message.channel.send(responseObject[message_content]);
+    //  If it contain "like if" react with 👍
+                    } else if (message_content.includes("like if")) {
+                        message.react("\u{1F44D}")
+    //  If it contain "jeff" react with a jeff emote
+                    } else if (message_content.includes("jeff")) {
+                        message.react("496028845967802378")
+                    }
+                }}
+        } catch(err) {
+            console.error(err)
+        }
+    })
 
 //  Very basic starboard
     client.on('messageReactionAdd', async (reaction, message) => {
