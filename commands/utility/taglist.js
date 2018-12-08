005cd1c46d909dbd45dd13b91fbb5b290d352bf7
Loic Bersier <loic.bersier1@gmail.com>
2018-12-08 04:58:14 +0100

    handle error

--- commands/utility/taglist.js
+++ commands/utility/taglist.js
@@ -17,23 +17,32 @@ module.exports = class taglistCommand extends Command {
         if(blacklist[message.author.id])
         return message.channel.send("You are blacklisted")
 
-        let customresponse = new SelfReloadJSON(`./tag/${message.guild.id}.json`);
-        let count = Object.keys(customresponse).length
-        
-            fs.readFile(`./tag/${message.guild.id}.json`, 'utf8', function readFileCallback(err, data){
-                if (err)
-                    console.log(err);
-                let json = JSON.stringify(data)
-                json = json.replace(/[{}"\\]+/g, '')
-                json = json.replace(/,+/g, '\n')
-                const tagEmbed = new Discord.RichEmbed()
-                .setColor("#ff9900")
-                .setTitle('Tags list')
-                .setDescription(`Trigger:Response\n\n${json}`)
-                .setFooter(`You have ${count} tags on this server`)
-        
-                message.say(tagEmbed);
-                
-            });
+        try {
+            let customresponse = new SelfReloadJSON(`./tag/${message.guild.id}.json`);
+            let count = Object.keys(customresponse).length
+            
+    
+                fs.readFile(`./tag/${message.guild.id}.json`, 'utf8', function readFileCallback(err, data){
+                    if (err) {
+                        console.log(err);
+                    }
+                    let json = JSON.stringify(data)
+                    json = json.replace(/[{}"\\]+/g, '')
+                    json = json.replace(/,+/g, '\n')
+                    const tagEmbed = new Discord.RichEmbed()
+                    .setColor("#ff9900")
+                    .setTitle('Tags list')
+                    .setDescription(`Trigger:Response\n\n${json}`)
+                    .setFooter(`You have ${count} tags on this server`)
+            
+                    message.say(tagEmbed);
+                });
+        } catch {
+            message.say('An error has occured, do you have any tags on the server?')
         }
+
+  
+
+        }
+
 };
