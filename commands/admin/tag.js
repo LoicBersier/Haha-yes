b45201af39164a856b9e4deaf97653e33c7ccae8
Loic Bersier <loic.bersier1@gmail.com>
2018-12-18 17:02:05 +0100

    moved to admin

---
+++ commands/admin/tag.js
@@ -0,0 +1,62 @@
+const { Command } = require('discord.js-commando');
+const blacklist = require('../../json/blacklist.json');
+const fs = require('fs');
+module.exports = class CustomResponseCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'tag',
+            aliases: ['customresponse'],
+            group: 'utility',
+            memberName: 'tag',
+            description: `Custom auto response`,
+            userPermissions: ['MANAGE_MESSAGES'],
+            args: [
+                {
+                    key: 'trigger',
+                    prompt: 'The word that will trigger the autoresponse (use "--" instead of spaces)',
+                    type: 'string',
+                },
+                {
+                    key: 'response',
+                    prompt: 'The response to the word ( you can use spaces here )',
+                    type: 'string',
+                }
+            ]
+        });
+    }
+
+    async run(message, { trigger, response }) {
+        if(blacklist[message.author.id])
+        return message.channel.send("You are blacklisted")
+
+            trigger = trigger.toLowerCase();
+            do {
+                trigger = trigger.replace('--', ' ')
+            } while (trigger.includes('--'))
+            
+            let customresponse = {}
+            let json = JSON.stringify(customresponse)
+
+            
+
+
+            fs.readFile(`./tag/${message.guild.id}.json`, 'utf8', function readFileCallback(err, data){
+                if (err){
+                    fs.writeFile(`./tag/${message.guild.id}.json`, `{"${trigger}": {"response":"${response}","owner":"${message.author.id}"}}`, function (err) {
+                        if (err){
+                            console.log(err);
+                        }
+                    })
+                } else {
+                customresponse = JSON.parse(data); //now it an object
+                customresponse.trigger = `response":${response},"owner":"${message.author.id}`
+                json = JSON.stringify(customresponse); //convert it back to json
+                fs.writeFile(`./tag/${message.guild.id}.json`, json, 'utf8', function(err) {
+                    if(err) {
+                        return console.log(err);
+                    } 
+            })}});
+            
+            return message.say(`autoresponse have been set to ${trigger} : ${response}`);
+        }
+};
