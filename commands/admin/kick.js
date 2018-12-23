da2caf72b1b0b37f6045993490215c300b28dcb1
Loic Bersier <loic.bersier1@gmail.com>
2018-12-23 05:41:50 +0100

    fixed it i think

--- commands/admin/kick.js
+++ commands/admin/kick.js
@@ -30,9 +30,9 @@ module.exports = class KickCommand extends Command {
             reasons = 'Nothing have been specified.'
         if(member.id === message.author.id)
             return message.say("Why would you kick yourself ?")
-        await member.send(`You have been kicked for the following reasons: "${reasons}"`);
+        await member.send(`You have been kicked for the following reasons: "${reasons}"`)
+        .error(message.say('Cant kick me fool'))
         member.kick(`Kicked by : ${message.author.username} for the following reasons : ${reasons}`)
         .then(() => message.reply(`${member.user.username} was succesfully kicked with the following reasons "${reasons}".`))
-        .error(message.say('Cant kick me fool'))
         };
 };
