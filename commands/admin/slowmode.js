1b88c0839820302f2998a9ba8fb0d2d364d45635
Loic Bersier <loic.bersier1@gmail.com>
2018-12-28 00:43:33 +0100

    Add slowmode

---
+++ commands/admin/slowmode.js
@@ -0,0 +1,26 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class CustomResponseCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'slowmode',
+            aliases: ['slow'],
+            group: 'admin',
+            memberName: 'slowmode',
+            description: `Custom auto response`,
+            userPermissions: ['MANAGE_CHANNELS'],
+            clientPermissions: ['MANAGE_CHANNELS'],
+            args: [
+                {
+                    key: 'slowmodeNumber',
+                    prompt: 'How many seconds should the slowmode be? ( 0 to remove it )',
+                    type: 'integer',
+                }
+            ]
+        });
+    }
+
+    async run(message, { slowmodeNumber }) {
+        message.channel.setRateLimitPerUser(slowmodeNumber);
+        }
+};
