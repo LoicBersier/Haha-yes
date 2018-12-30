fd8f56ad1df1fe29880818db773567026f3ceca4
Loic Bersier <loic.bersier1@gmail.com>
2018-12-30 03:35:25 +0100

    dm/pm users

---
+++ commands/owner/dm.js
@@ -0,0 +1,45 @@
+const { Command } = require('discord-akairo');
+
+class EvalCommand extends Command {
+    constructor() {
+        super('dm', {
+            aliases: ['dm', 'pm'],
+            split: 'none',
+            category: 'owner',
+            args: [
+                {
+                    id: 'user',
+                    type: 'user'
+                },
+                {
+                    id: 'message',
+                    type: 'string'
+                }
+            ],
+            ownerOnly: 'true',
+            description: {
+				content: 'DM users',
+				usage: '[user id] [message]',
+				examples: ['267065637183029248 hello i recived your feedback and...']
+			}
+        });
+    }
+
+    async exec(message, args) {
+        let user = args.user;
+        let message = args.message;
+        
+        let Attachment = (message.attachments).array();
+        if (Attachment[0]) {
+            user.send(`**Message from the dev:**\n${message}\n${Attachment[0].url}`)
+            message.say(`DM sent to ${user.username}`)
+        }
+        else {
+            user.send(`**Message from the dev:**\n${message}`)
+            message.say(`DM sent to ${user.username}`)
+        }
+
+    }
+}
+
+module.exports = EvalCommand;
