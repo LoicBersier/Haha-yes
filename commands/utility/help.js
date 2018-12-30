6661a110f61ad4b17ccdbd0e901b02697673cb95
Loic Bersier <loic.bersier1@gmail.com>
2018-12-30 01:57:32 +0100

    updated examples

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -22,7 +22,7 @@ class HelpCommand extends Command {
 			description: {
 				content: 'Displays a list of commands or information about a command.',
 				usage: '[command]',
-				examples: ['', 'star', 'remove-rep']
+				examples: ['', 'say', 'tag']
 			}
 		});
 	}
