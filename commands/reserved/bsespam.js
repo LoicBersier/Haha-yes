69d18944e2e62a7190e83807341da41f961fdcfb
Supositware <loic.bersier1@gmail.com>
2018-12-31 21:00:02 +0100

    added bsespam

---
+++ commands/reserved/bsespam.js
@@ -0,0 +1,41 @@
+const { Command } = require('discord-akairo');
+
+class bsespamCommand extends Command {
+    constructor() {
+        super('bsespam', {
+            aliases: ['bsespam'] ,   //Required
+            category: 'reserved',    //recommended
+            channelRestriction: 'guild',      //needed if you want to restrict where we can launch the command
+            args: [                       //if need args
+                {
+                    id: 'number',
+                    type: 'interger',
+                },
+                {
+                    id: 'text',
+                    type: 'string'
+                }
+            ],
+            description: {               //recommended 
+                content: 'ONLY FOR BIG SNOW ENERGY\nSpam the text you send',
+                usage: '[args]',
+                examples: ['']
+            }
+        });
+    }
+
+    async exec(message, args) {
+        if (message.author.id != "428387534842626048")
+            return message.say('Command only available to **Big Snow Energy**')
+        if (args.number <= 10) {
+            for(let i = 0; i < args.number; i++) {
+                message.channel.send(args.text);
+                }
+                return message.channel.send('Finished :)');
+        } else {
+            return message.channel.send('No more than 10!')
+        }
+    }
+}
+
+module.exports = bsespamCommand;
