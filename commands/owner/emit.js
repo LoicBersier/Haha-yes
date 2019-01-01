4f1c830e060fa17876c55e98ef27c3a66b840160
Supositware <loic.bersier1@gmail.com>
2019-01-01 03:49:28 +0100

    emit event

---
+++ commands/owner/emit.js
@@ -0,0 +1,31 @@
+const { Command } = require('discord-akairo');
+
+class emitCommand extends Command {
+    constructor() {
+        super('emit', {
+            aliases: ['emit'],
+            split: 'none',
+            category: 'owner',
+            ownerOnly: 'true',
+            args: [
+                {
+                    id: 'event',
+                    prompt: 'Wich event should i trigger?',
+                    type: 'string'
+                }
+            ],
+            description: {
+				content: 'Trigger an event',
+				usage: '[event]',
+				examples: ['ready']
+			}
+        });
+    }
+
+    async exec(message, args) {
+        this.client.emit(`${args.event}`);
+        return message.channel.send(`${args.event} has been emited!`)
+    }
+}
+
+module.exports = emitCommand;
