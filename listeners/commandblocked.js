a457930fa5b32f361a580f06670d813fe2bb8cde
Supositware <loic.bersier1@gmail.com>
2019-01-01 03:49:23 +0100

    Add different message

--- listeners/commandblocked.js
+++ listeners/commandblocked.js
@@ -10,7 +10,20 @@ class CommandBlockedListener extends Listener {
 
     exec(message, command, reason) {
         console.log(`${message.author.username} was blocked from using ${command.id} because of ${reason}!`);
-        return message.reply(`You can't use this command beacause of ${reason}`);
+        switch(reason) {
+            case "Owner":
+                message.reply('Nice try but you aren\'t the owner <a:memed:433320880135733248>');
+                break;
+            case "clientPermissions":
+                message.reply('Im missing the required permissions for this command!');
+                break;
+            case "userPermissions":
+                message.reply('You are missing some permissions to use this command!');
+                break;
+            case "blacklist": 
+                message.reply('You can\'t use this command because you have been blacklisted!')
+            
+        }
     }
 }
 
