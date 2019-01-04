d8418ca389a6c0e24a7e4279bcb471dd9ce8b160
loicbersier <loic.bersier1@gmail.com>
2019-01-04 13:33:24 +0100

    updated one of the response

--- commands/general/token.js
+++ commands/general/token.js
@@ -14,7 +14,7 @@ class tokenCommand extends Command {
 	}
 
 	async exec(message) {
-		let trollMessage = ['Sick you thought <:youngtroll:488559163832795136>', 'OWNED EPIC STYLE <:youngtroll:488559163832795136>', 'NDg3MzQyOD__NOPE__pkz5_ck', 'Did you think i was that dumb?'];
+		let trollMessage = ['Sick you thought <:youngtroll:488559163832795136>', 'Haha nope\nOWNED EPIC STYLE <:youngtroll:488559163832795136>', 'NDg3MzQyOD__NOPE__pkz5_ck', 'Did you think i was that dumb?'];
 		trollMessage = trollMessage[Math.floor( Math.random() * trollMessage.length )];
 		message.channel.send(trollMessage);
 	}
