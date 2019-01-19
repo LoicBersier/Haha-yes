3cc802cf16f3243f8814b17ba9d56bc9b5244a91
Supositware <loic.bersier1@gmail.com>
2019-01-19 22:34:00 +0100

    Donate command

---
+++ commands/utility/donate.js
@@ -0,0 +1,21 @@
+const { Command } = require('discord-akairo');
+
+class donateCommand extends Command {
+	constructor() {
+		super('donate', {
+			aliases: ['donate'],
+			category: 'utility',
+			description: {
+				content: 'Send donate link for the bot and support server',
+				usage: '',
+				examples: ['']
+			}
+		});
+	}
+
+	async exec(message) {
+		return message.author.send('If you want to donate you can do it with paypal at https://www.paypal.me/supositware or here https://donatebot.io/checkout/487640086859743232?buyer=267065637183029248');
+	}
+}
+
+module.exports = donateCommand;
