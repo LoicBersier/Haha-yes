1067e3766e339cabca1cc09d638bd21d9765aa3e
loicbersier <loic.bersier1@gmail.com>
2019-01-19 18:55:09 +0100

    EPIC TF2 PREMIUM FEATURE

---
+++ commands/hidden/premium.js
@@ -0,0 +1,22 @@
+const { Command } = require('discord-akairo');
+
+class premiumCommand extends Command {
+	constructor() {
+		super('premium', {
+			aliases: ['premium'],
+			prefix: '!',
+			category: 'hidden',
+			description: {
+				content: 'premium',
+				usage: '',
+				examples: ['']
+			}
+		});
+	}
+
+	async exec(message) {
+		message.channel.send('EPIC TF2 FEATURE HERE !!!!!!111111 YOU CAN FLY AND CHEAT !!!!!!!!!!!!!!!!! ALSO PREMIUM SONG AND ANNOY EVERYONE AND FLEX ON EM HATERS WITH CUSTOM SKIN!!!!');
+	}
+}
+
+module.exports = premiumCommand;
