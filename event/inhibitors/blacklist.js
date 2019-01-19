fb68b205a6b651e423aaa80003c78db346aba308
loicbersier <loic.bersier1@gmail.com>
2019-01-19 19:01:41 +0100

    async

--- event/inhibitors/blacklist.js
+++ event/inhibitors/blacklist.js
@@ -7,7 +7,7 @@ class BlacklistInhibitor extends Inhibitor {
 		});
 	}
 
-	exec(message) {
+	async exec(message) {
 		const blacklist = ['501856229123948545', '497730155691638784'];
 		return blacklist.includes(message.author.id);
 	}
