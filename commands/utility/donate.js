43e4378d324fb7fc48767889596b831443ca66d9
Supositware <loic.bersier1@gmail.com>
2019-01-20 16:50:17 +0100

    Added patreon link

--- commands/utility/donate.js
+++ commands/utility/donate.js
@@ -14,7 +14,7 @@ class donateCommand extends Command {
 	}
 
 	async exec(message) {
-		return message.channel.send('If you want to donate you can do it with paypal at https://www.paypal.me/supositware or here https://donatebot.io/checkout/487640086859743232?buyer=267065637183029248');
+		return message.channel.send('If you want to donate you can do it with paypal at https://www.paypal.me/supositware, https://donatebot.io/checkout/487640086859743232?buyer=267065637183029248 or if you prefer on patreon! https://www.patreon.com/user?u=15330358&utm_medium=social&utm_source=twitter&utm_campaign=creatorshare');
 	}
 }
 
