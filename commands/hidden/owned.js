44d5444e81f54597f9d843130acbb5fce4bce63c
Supositware <loic.bersier1@gmail.com>
2019-02-25 00:08:29 +0100

    new owned

--- commands/hidden/owned.js
+++ commands/hidden/owned.js
@@ -14,7 +14,7 @@ class OwnedCommand extends Command {
 	}
 
 	async exec(message) {
-		let ownedMessage = ['TROLOLOLO OWNED EPIC STYLE', 'Owned noob', 'HAHA BRO YOU JUST GOT OOOOOOOOWNED HAHAHAHAHHAHA NOOOB NOOOOB NOOOB OWNED NOOB<:youngtroll:488559163832795136>', '<a:op:516341492982218756> op op op owned epic style <a:op:516341492982218756>'];
+		let ownedMessage = ['TROLOLOLO OWNED EPIC STYLE', 'Owned noob', 'HAHA BRO YOU JUST GOT OOOOOOOOWNED HAHAHAHAHHAHA NOOOB NOOOOB NOOOB OWNED NOOB<:youngtroll:488559163832795136>', '<a:op:516341492982218756> op op op owned epic style <a:op:516341492982218756>', 'HAHAHA BRO YOU HAVE BEEN OWNED TROLL BRO STYLE'];
 		ownedMessage = ownedMessage[Math.floor( Math.random() * ownedMessage.length )];
 		message.channel.send(ownedMessage);
 	}
