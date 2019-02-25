656bfc5ce19a07f7d058e2d8b856817cef93639d
Supositware <loic.bersier1@gmail.com>
2019-02-26 00:23:54 +0100

    command to censor/uncensor

---
+++ commands/owner/twitter/censor.js
@@ -0,0 +1,57 @@
+const { Command } = require('discord-akairo');
+const fs = require('fs');
+
+class censorCommand extends Command {
+	constructor() {
+		super('censor', {
+			aliases: ['censor'],
+			category: 'owner',
+			ownerOnly: 'true',
+			args: [
+				{
+					id: 'word',
+					type: 'string'
+				}
+			],
+			description: {
+				content: 'Censor word for twitter',
+				usage: '[word]',
+				examples: ['nigger']
+			}
+		});
+	}
+
+	async exec(message, args) {
+		let word = args.word;
+
+		word = word.toLowerCase();
+
+		let words = [];
+		let json = JSON.stringify(words);
+
+		fs.readFile('./json/twitter/censor.json', 'utf8', function readFileCallback(err, data) {
+			if (err) {
+				fs.writeFile('./json/twitter/censor.json', `["${word}"]`, function (err) {
+					if (err) {
+						
+						console.log(err);
+					}
+				});
+			} else {
+				words = JSON.parse(data); //now it an object
+				words.push(word);
+				json = JSON.stringify(words); //convert it back to json
+				fs.writeFile('./json/twitter/censor.json', json, 'utf8', function (err) {
+					if (err) {
+						return console.log(err);
+					}
+				});
+			}
+		});
+
+		
+		return message.channel.send(`censored the word ${word}`);
+	}
+}
+
+module.exports = censorCommand;
---
+++ commands/owner/twitter/uncensor.js
@@ -0,0 +1,57 @@
+const { Command } = require('discord-akairo');
+const fs = require('fs');
+
+class uncensorCommand extends Command {
+	constructor() {
+		super('uncensor', {
+			aliases: ['uncensor'],
+			category: 'owner',
+			ownerOnly: 'true',
+			args: [
+				{
+					id: 'word',
+					type: 'string'
+				}
+			],
+			description: {
+				content: 'Unensor word for twitter',
+				usage: '[word]',
+				examples: ['shit']
+			}
+		});
+	}
+
+	async exec(message, args) {
+		let word = args.word;
+
+		word = word.toLowerCase();
+
+		let words = [];
+		let json = JSON.stringify(words);
+
+		fs.readFile('./json/twitter/uncensor.json', 'utf8', function readFileCallback(err, data) {
+			if (err) {
+				fs.writeFile('./json/twitter/uncensor.json', `["${word}"]`, function (err) {
+					if (err) {
+						
+						console.log(err);
+					}
+				});
+			} else {
+				words = JSON.parse(data); //now it an object
+				words.push(word);
+				json = JSON.stringify(words); //convert it back to json
+				fs.writeFile('./json/twitter/uncensor.json', json, 'utf8', function (err) {
+					if (err) {
+						return console.log(err);
+					}
+				});
+			}
+		});
+
+		
+		return message.channel.send(`Uncensored the word ${word}`);
+	}
+}
+
+module.exports = uncensorCommand;
