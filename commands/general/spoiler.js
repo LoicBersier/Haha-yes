892c8e382d15900de3a63cd3f8e0c7e62e64ebde
Supositware <loic.bersier1@gmail.com>
2019-03-07 22:50:40 +0100

    Put text in spoiler

---
+++ commands/general/spoiler.js
@@ -0,0 +1,35 @@
+const { Command } = require('discord-akairo');
+
+class spoilerCommand extends Command {
+	constructor() {
+		super('spoiler', {
+			aliases: ['spoiler'],
+			category: 'general',
+			args: [
+				{
+					id: 'text',
+					type: 'string',
+					match: 'rest'
+				}
+			],
+			description: {
+				content: 'Replace every letter in your text with letter spoiler',
+				usage: 'this is epic',
+				examples: ['this is epic']
+			}
+		});
+	}
+
+	async exec(message, args) {
+		let text = args.text;
+		if (!text)
+			return;
+
+		text = text.split('').join('||||');
+
+		//	  Send the final text
+		return message.channel.send('||' + text + '||');
+	}
+}
+
+module.exports = spoilerCommand;
