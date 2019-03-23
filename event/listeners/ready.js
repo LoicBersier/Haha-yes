a1b712a13d5d4dfc903d3a8900f063f395b06dff
Supositware <loic.bersier1@gmail.com>
2019-03-23 19:28:57 +0100

    Choose between playing and watching status at random on start

--- event/listeners/ready.js
+++ event/listeners/ready.js
@@ -1,6 +1,7 @@
 const { Listener } = require('discord-akairo');
 const { prefix, statsChannel } = require('../../config.json');
-
+const game = require('../../json/status/playing.json');
+const watch = require('../../json/status/watching.json');
 
 class ReadyListener extends Listener {
 	constructor() {
@@ -14,9 +15,24 @@ class ReadyListener extends Listener {
 		//  Send stats to the console
 		console.log(`\x1b[32mLogged in as \x1b[34m${this.client.user.tag}\x1b[0m! (\x1b[33m${this.client.user.id}\x1b[0m)`);
 		console.log(`Ready to serve in \x1b[33m${this.client.channels.size}\x1b[0m channels on \x1b[33m${this.client.guilds.size}\x1b[0m servers, for a total of \x1b[33m${this.client.users.size}\x1b[0m users. \x1b${this.client.readyAt}\x1b[0m`);
+
+		//Bot status
+		if (Math.floor((Math.random() * 2) + 1) == 1) {
+			console.log('Status type: Watching');
+
+			let status = watch[Math.floor((Math.random() * watch.length))];
+			status = status.replace('${prefix}', prefix);
 		
-		//Bot activity
-		this.client.user.setActivity(`${prefix} feedback <feedback> to tell me what you think of the bot! | ${prefix} help`);
+			this.client.user.setActivity(`${status} | ${prefix} help`, { type: 'WATCHING' });
+		} else {
+			console.log('Status type: Playing');
+
+			let status = game[Math.floor((Math.random() * game.length))];
+			status = status.replace('${prefix}', prefix);
+		
+			this.client.user.setActivity(`${status} | ${prefix} help`, { type: 'PLAYING' });
+		}
+
 
 		//  Send stats to the 'stats' channel in the support server if its not the test bot
 		if (this.client.user.id == 377563711927484418) {
