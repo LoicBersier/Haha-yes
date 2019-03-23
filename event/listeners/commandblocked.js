64d56ddba76ff8c68da2d9dd07654b65cc1da6bc
Supositware <loic.bersier1@gmail.com>
2019-03-23 23:55:52 +0100

    added random message to blacklisted user & a message to blacklisted server

--- event/listeners/commandblocked.js
+++ event/listeners/commandblocked.js
@@ -11,6 +11,7 @@ class CommandBlockedListener extends Listener {
 	async exec(message, command, reason) {
 		console.log(`${message.author.username} was blocked from using ${command.id} because of ${reason}!`);
 		let ownerMessage;
+		let blacklistMessage;
 		switch(reason) {
 		case 'owner':
 			ownerMessage = ['Nice try but you aren\'t the owner <a:memed:433320880135733248>', 'LOADING SUPER SECRET COMMAND <a:loadingmin:527579785212329984> Wait a minute... you aren\'t the owner!', 'uhm, how about no'];
@@ -24,9 +25,11 @@ class CommandBlockedListener extends Listener {
 			message.reply('You can\'t use this command in DM!');
 			break;
 		case 'blacklist': 
-			message.reply('You can\'t use this command because you have been blacklisted!');
-			break;
+			blacklistMessage = ['bro... i think you are blacklisted.... OWNED!!!', 'You can\'t use this command because you have been blacklisted!',' you are blacklisted!!!1111!! be less naughty next time!', 'blacklisted,,,,,, lol owned bro'];
+			blacklistMessage = blacklistMessage[Math.floor( Math.random() * blacklistMessage.length )];
+			message.reply(blacklistMessage);			break;
 		case 'serverblacklist': 
+			message.channel.send('This server have been blacklisted... to appeal contact Supositware#1616, and now i will yeet out of here');
 			message.guild.leave();
 			break;
 		}
