0b0852c141a02df7147d55a2df96c898b02f29de
Supositware <loic.bersier1@gmail.com>
2019-04-06 21:34:39 +0200

    made it available for everyone

--- commands/utility/play.js
+++ commands/utility/play.js
@@ -6,7 +6,6 @@ class playCommand extends Command {
 		super('play', {
 			aliases: ['play'],
 			category: 'utility',
-			ownerOnly: 'true',
 			args: [
 				{
 					id: 'ytblink',
@@ -15,7 +14,7 @@ class playCommand extends Command {
 				}
 			],
 			description: {
-				content: 'Play play from the link you send ( ONLY FOR TESTING NOW )',
+				content: 'play music from the link you send ( This command might or might not stay in the bot, as it is basic, if people abuse it e.g: making play 10 hours song just for the purpose of making the bot slow, i will remove this command! )',
 				usage: '[youtube link]',
 				examples: ['https://www.youtube.com/watch?v=mzHWLLn5Z4A']
 			}
