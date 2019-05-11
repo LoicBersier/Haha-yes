e660b238a4bf67d689b5c9afe1d1ea5fc5610436
Loïc Bersier <supositware@Loics-MacBook-Pro.local>
2019-05-11 20:56:36 +0200

    blacklisted wrong user

--- event/inhibitors/blacklist.js
+++ event/inhibitors/blacklist.js
@@ -8,7 +8,7 @@ class BlacklistInhibitor extends Inhibitor {
 	}
 
 	async exec(message) {
-		const blacklist = ['501856229123948545', '497730155691638784', '215681880899584000', '29476879240658944'];
+		const blacklist = ['501856229123948545', '497730155691638784', '29476879240658944'];
 		return blacklist.includes(message.author.id);
 	}
 }
