7938d45e36e7282749945c967b1f1870f6d9de85
loicbersier <loic.bersier1@gmail.com>
2019-07-09 05:02:25 +0200

    put clear message in terminal when shutdown

--- commands/owner/reboot.js
+++ commands/owner/reboot.js
@@ -15,7 +15,9 @@ class RebootCommand extends Command {
 	}
 
 	async exec(message) {
+		console.log('\x1b[31m\x1b[47m\x1b[5mSHUTING DOWN!!!!!\x1b[0m');
 		await message.channel.send('k bye thx', { files: ['https://cdn.discordapp.com/attachments/532980995767533568/553656409452183552/bad_things_happen_to_good_people.jpg', 'https://cdn.discordapp.com/attachments/532980995767533568/553310025792356362/meme.png']});
+		console.log('\x1b[31m\x1b[47m\x1b[5mSHUTING DOWN!!!!!\x1b[0m');
 		process.exit();
 
 	}
