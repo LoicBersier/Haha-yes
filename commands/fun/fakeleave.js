dcdb0190d7d764ac9a17fa7e738124d4bfbc0aa7
loicbersier <loic.bersier1@gmail.com>
2019-08-11 19:54:41 +0200

    Fake leave message

---
+++ commands/fun/fakeleave.js
@@ -0,0 +1,70 @@
+const { Command } = require('discord-akairo');
+const fs = require('fs');
+const rand = require('../../rand.js');
+
+class fakeleaveCommand extends Command {
+	constructor() {
+		super('fakeleave', {
+			aliases: ['fakeleave'],
+			category: 'admin',
+			channelRestriction: 'guild',
+			args: [
+				{
+					id: 'member',
+					type: 'member',
+					match: 'rest'
+				}
+			],
+			description: {
+				content: 'Fake leave message',
+				usage: '[user]',
+				examples: ['Supositware']
+			}
+		});
+	}
+
+	async exec(message, args) {
+		if (fs.existsSync(`./bye/${message.guild.id}.json`)) {
+			let member;
+			if (args.member) {
+				member = args.member;
+			} else {
+				member = message.author;
+			}
+
+			let bye = require(`../../bye/${message.guild.id}.json`);
+
+			const channel = this.client.channels.get(bye['channel']);
+
+			let byeMessage = bye['message'];
+
+			byeMessage = byeMessage.replace(/\[member\]/, member);
+			byeMessage = byeMessage.replace(/\[server\]/, message.guild.name);
+
+			let attach;
+			if (byeMessage.includes('[attach:')) {
+				attach = byeMessage.split(/(\[attach:.*?])/);
+				for (let i = 0, l = attach.length; i < l; i++) {
+					if (attach[i].includes('[attach:')) {
+						attach = attach[i].replace('[attach:', '').slice(0, -1);
+						i = attach.length;
+					}
+				}
+				byeMessage = byeMessage.replace(/(\[attach:.*?])/, '');
+			}
+
+			byeMessage = rand.random(byeMessage);	
+
+
+			if (attach) {
+				return channel.send(byeMessage, {files: [attach]});
+			} else {
+				return channel.send(byeMessage);
+			}
+		} else {
+			return message.channel.send('The server need a leave message first!');
+		}
+	}
+}
+
+module.exports = fakeleaveCommand;
