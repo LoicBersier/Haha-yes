8166a16a1754d19f8f11123bbe86b6b45bb705d3
loicbersier <loic.bersier1@gmail.com>
2019-08-12 13:53:37 +0200

    delete the message, not the channel

--- commands/fun/fakebot.js
+++ commands/fun/fakebot.js
@@ -50,7 +50,7 @@ class fakebotCommand extends Command {
 					name: args.member.username,
 					avatar: args.member.displayAvatarURL()
 				});
-				message.channel.delete();
+				message.delete();
 				return webhook.send(args.message);
 			});
 	}
