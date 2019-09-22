72c569af1d0217eee2a6660d46ee3b71b11d0c31
loicbersier <loic.bersier1@gmail.com>
2019-09-22 17:50:56 +0200

    Get comic from http://explosm.net/rcg

---
+++ commands/fun/explosm.js
@@ -0,0 +1,24 @@
+const { Command } = require('discord-akairo');
+const extract = require('meta-extractor');
+
+class explosmCommand extends Command {
+	constructor() {
+		super('explosm', {
+			aliases: ['explosm', 'rcg'],
+			category: 'fun',
+			description: {
+				content: 'Comic randomly generated from http://explosm.net/rcg',
+				usage: '',
+				examples: ['']
+			}
+		});
+	}
+
+	async exec(message) {
+		extract({ uri: 'http://explosm.net/rcg' }, (err, res) => {
+			return message.channel.send({files: [res.ogImage]});
+		});
+	}
+			
+}
+module.exports = explosmCommand;
