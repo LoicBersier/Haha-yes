506202f30c7bec9f92a8cc54f0c6174ed34cdc72
loicbersier <loic.bersier1@gmail.com>
2019-10-05 20:06:52 +0200

    show the author as what is set in config.json

--- commands/utility/about.js
+++ commands/utility/about.js
@@ -1,5 +1,6 @@
 const { Command } = require('discord-akairo');
 const { MessageEmbed } = require('discord.js');
+const { ownerID } = require('../../config.json');
 
 class aboutCommand extends Command {
 	constructor() {
@@ -17,7 +18,7 @@ class aboutCommand extends Command {
 	async exec(message) {
 		const aboutEmbed = new MessageEmbed()
 			.setColor('#ff9900')
-			.setAuthor('Supositware#1616', this.client.user.avatarURL)
+			.setAuthor(`${this.client.users.get(ownerID).username}#${this.client.users.get(ownerID).discriminator} (${ownerID})`, this.client.user.avatarURL)
 			.setTitle('About me')
 			.setURL('https://gitlab.com/LoicBersier/DiscordBot')
 			.setDescription('This bot is made using [discord.js](https://github.com/discordjs/discord.js) & [Discord-Akairo](https://github.com/discord-akairo/discord-akairo)\nHelp command from [hoshi](https://github.com/1Computer1/hoshi)\n* [Rantionary](https://github.com/RantLang/Rantionary) for there dictionnary.\nThanks to Tina the Cyclops girl#5759 for inspiring me for making this bot!')
