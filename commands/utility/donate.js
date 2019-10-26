c5613385f5b0bd292f59e43c8d46b14f444afcf7
loicbersier <loic.bersier1@gmail.com>
2019-10-27 00:26:28 +0200

    change the look of the command and add more aliases

--- commands/utility/donate.js
+++ commands/utility/donate.js
@@ -1,9 +1,10 @@
 const { Command } = require('discord-akairo');
+const { MessageEmbed } = require('discord.js');
 
 class donateCommand extends Command {
 	constructor() {
 		super('donate', {
-			aliases: ['donate'],
+			aliases: ['donate', 'donation', 'giveallmymoney', 'givemoney'],
 			category: 'utility',
 			description: {
 				content: 'Send donate link for the bot and support server',
@@ -14,7 +15,12 @@ class donateCommand extends Command {
 	}
 
 	async exec(message) {
-		return message.channel.send('If you want to donate you can do it with paypal at https://www.paypal.me/supositware, https://donatebot.io/checkout/487640086859743232?buyer=267065637183029248 or if you prefer on patreon! https://www.patreon.com/user?u=15330358&utm_medium=social&utm_source=twitter&utm_campaign=creatorshare');
+		const Embed = new MessageEmbed()
+			.setColor('#ff9900')
+			.setTitle('Donation link')
+			.setDescription('[Paypal](https://www.paypal.me/supositware)\n[Patreon](https://www.patreon.com/bePatron?u=15330358)');
+
+		message.channel.send(Embed);
 	}
 }
 
