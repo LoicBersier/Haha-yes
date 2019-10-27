8c585951aa21b60d916e5bfc8870eb4bacdc1fcc
loicbersier <loic.bersier1@gmail.com>
2019-10-27 13:15:25 +0100

    fixed typo and made it show "no one :(" when there is no entry in the donator table

--- commands/utility/about.js
+++ commands/utility/about.js
@@ -19,11 +19,17 @@ class aboutCommand extends Command {
 	async exec(message) {
 		const Donator = await donator.findAll({order: ['id']});
 
-		let description = 'This bot is made using [discord.js](https://github.com/discordjs/discord.js) & [Discord-Akairo](https://github.com/discord-akairo/discord-akairo)\nHelp command from [hoshi](https://github.com/1Computer1/hoshi)\n* [Rantionary](https://github.com/RantLang/Rantionary) for there dictionnary.\nThanks to Tina the Cyclops girl#5759 for inspiring me for making this bot!\n\nThe people who donated for the bot <3\n';
+		let description = 'This bot is made using [discord.js](https://github.com/discordjs/discord.js) & [Discord-Akairo](https://github.com/discord-akairo/discord-akairo)\nHelp command from [hoshi](https://github.com/1Computer1/hoshi)\n[Rantionary](https://github.com/RantLang/Rantionary) for their dictionnary.\nThanks to Tina the Cyclops girl#5759 for inspiring me for making this bot!\n\nThe people who donated for the bot <3\n';
 		
-		for (let i = 0; i < Donator.length; i++) {
-			description += `${this.client.users.get(Donator[i].get('userID')).username}#${this.client.users.get(Donator[i].get('userID')).discriminator} (${Donator[i].get('userID')}) ${Donator[i].get('comment')}\n`;
+		if (Donator[0]) {
+			for (let i = 0; i < Donator.length; i++) {
+				description += `**${this.client.users.get(Donator[i].get('userID')).username}#${this.client.users.get(Donator[i].get('userID')).discriminator} (${Donator[i].get('userID')}) ${Donator[i].get('comment')}**\n`;
+			}
+		} else {
+			description += 'No one :(';
 		}
+
+
 
 
 
