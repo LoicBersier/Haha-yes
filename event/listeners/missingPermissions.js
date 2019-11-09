2c43a6835e8dbeebc48abecb2bb24fdf9a20ba86
loicbersier <loic.bersier1@gmail.com>
2019-11-09 12:14:54 +0100

    Send the missing perm in DM if the bot can't send messages

--- event/listeners/missingPermissions.js
+++ event/listeners/missingPermissions.js
@@ -9,14 +9,21 @@ class missingPermissionsListener extends Listener {
 	}
 
 	async exec(message, command, type, missing) {
-		if (missing == 'SEND_MESSAGES') return; // If bot can't send messages just do nothing
-		
 		switch(type) {
 		case 'client':
-			message.reply(`Im missing the required permissions for this command!, \`${missing}\``);
+			if (missing == 'SEND_MESSAGES') {
+				return message.author.send(`Im missing the required permissions for this command!, \`${missing}\``);
+			} else {
+				message.reply(`Im missing the required permissions for this command!, \`${missing}\``);
+			}
 			break;
 		case 'user':
-			message.reply(`You are missing some permissions to use this command!, \`${missing}\``);
+			if (missing == 'SEND_MESSAGES') {
+				return message.author.send(`You are missing some permissions to use this command!, \`${missing}\``);
+			} else {
+				message.reply(`You are missing some permissions to use this command!, \`${missing}\``);
+			}
+			
 			break;
 		}
 	}
