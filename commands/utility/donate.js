729432fa9a1f113f6fdb38283ab7e642ec302a4b
loicbersier <loic.bersier1@gmail.com>
2019-12-08 12:59:03 +0100

    Added eth wallet address

--- commands/utility/donate.js
+++ commands/utility/donate.js
@@ -18,7 +18,7 @@ class donateCommand extends Command {
 		const Embed = this.client.util.embed()
 			.setColor(message.member.displayHexColor)
 			.setTitle('Donation link')
-			.setDescription('If you decide to donate, please use the feedback command to let the owner know about it so he can put you in the about and donator command\n[Paypal](https://www.paypal.me/supositware)\n[Patreon](https://www.patreon.com/bePatron?u=15330358)\n[Brave referal program](https://brave.com/hah459)\nTip me with Brave BAT token on [my website](https://namejeff.xyz/)');
+			.setDescription('If you decide to donate, please use the feedback command to let the owner know about it so he can put you in the about and donator command\n[Paypal](https://www.paypal.me/supositware)\n[Patreon](https://www.patreon.com/bePatron?u=15330358)\n[Brave referal program](https://brave.com/hah459)\nTip me with Brave BAT token on [my website](https://namejeff.xyz/)\nYou can also donate ETH to ``0xe188F9062A74cc29e23D0602F4Fe335B1F5D8409``');
 
 		return message.channel.send(Embed);
 	}
