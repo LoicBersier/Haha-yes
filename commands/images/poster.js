971ce9bfed25d8bec9424fa02fe62b653d2dec7f
loicbersier <loic.bersier1@gmail.com>
2020-02-22 23:53:07 +0100

    changed command description

--- commands/images/poster.js
+++ commands/images/poster.js
@@ -50,8 +50,8 @@ class posterCommand extends Command {
 				},
 			],
 			description: {
-				content: 'Create demotivational poster (use ``|`` to separate top text and bottom text) WIP',
-				usage: '[link to image] [topText|bottomText] (optional) [--width number] [--height number]',
+				content: 'Create demotivational poster (use ``|`` to separate top text and bottom text) WIP\nCan make use of --width, --border, --titleSize and --subtextSize to fine tune the image',
+				usage: '[link to image] [topText|bottomText]',
 				examples: ['https://cdn.discordapp.com/attachments/484013245158522909/679686216903426104/0nhsxje5vfg41-1.jpg NO PLEASE DON\'T | My mom said im not allowed to']
 			}
 		});
