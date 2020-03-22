fa9f2437ed704f536ee62af3888445e424c0a54f
loicbersier <loic.bersier1@gmail.com>
2020-03-22 20:18:40 +0100

    choose one random word

---
+++ commands/general/randomizer.js
@@ -0,0 +1,34 @@
+const { Command } = require('discord-akairo');
+
+class randomizerCommand extends Command {
+	constructor() {
+		super('randomizer', {
+			aliases: ['randomizer'],
+			category: 'general',
+			clientPermissions: ['SEND_MESSAGES'],
+			args: [
+				{
+					id: 'text',
+					type: 'string',
+					prompt: {
+						start: 'Need words to randomize',
+					},
+					match: 'rest'
+				}
+			],
+			description: {
+				content: 'Choose one random word',
+				usage: '[multiples words]',
+				examples: ['Hello bye']
+			}
+		});
+	}
+
+	async exec(message, args) {
+		let words = args.text.split(' ');
+
+		return message.channel.send(words[Math.floor((Math.random() * words.length))]);
+	}
+}
+
+module.exports = randomizerCommand;
