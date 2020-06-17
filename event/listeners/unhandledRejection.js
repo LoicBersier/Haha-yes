890d294f9d241aa7582fedf831c3da55fda31033
loicbersier <loic.bersier1@gmail.com>
2020-06-17 15:29:09 +0200

    output reason

--- event/listeners/unhandledRejection.js
+++ event/listeners/unhandledRejection.js
@@ -8,7 +8,8 @@ class unhandledRejectionListener extends Listener {
 		});
 	}
 
-	async exec(error) {
+	async exec(error, reason) {
+		console.error(reason);
 		return console.error(`\x1b[31mUncaught Promise Rejection: ${error}\x1b[37m`);
 	}
 }
