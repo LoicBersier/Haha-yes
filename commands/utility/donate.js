0e4d11e1533a588c1b7d0f2e29cb490644698755
loicbersier <loic.bersier1@gmail.com>
2020-11-25 15:27:25 +0100

    its BAT not brave token idiot
    Signed-off-by: loicbersier <loic.bersier1@gmail.com>

--- commands/utility/donate.js
+++ commands/utility/donate.js
@@ -18,7 +18,7 @@ class donateCommand extends Command {
 		const Embed = this.client.util.embed()
 			.setColor(message.member ? message.member.displayHexColor : 'NAVY')
 			.setTitle('Donation link')
-			.setDescription('If you decide to donate, please use the feedback command to let the owner know about it so he can put you in the about and donator command\n[Paypal](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KEMESDXL8Q5YY&source=url)\nYou can give [Brave token](https://namejeff.xyz) on my website');
+			.setDescription('If you decide to donate, please use the feedback command to let the owner know about it so he can put you in the about and donator command\n[Paypal](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KEMESDXL8Q5YY&source=url)\nYou can give [BAT token](https://namejeff.xyz) on my website');
 
 		return message.channel.send(Embed);
 	}
