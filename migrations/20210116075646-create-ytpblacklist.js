93b278e86c81f3fd865601b1500b535c928511bc
loicbersier <loic.bersier1@gmail.com>
2021-01-16 09:03:34 +0100

    forgot to add ytpblacklist migration file
    Signed-off-by: loicbersier <loic.bersier1@gmail.com>

---
+++ migrations/20210116075646-create-ytpblacklist.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('ytpblacklists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userID: {
+        type: Sequelize.BIGINT
+      },
+      reason: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('ytpblacklists');
+  }
+};
