7926820e70bc66c4504f1c63d571e971e01ed2c4
Supositware <sup@libtar.de>
2024-08-31 15:44:31 +0200

    Fix download format

--- commands/utility/vid2gif.js
+++ commands/utility/vid2gif.js
@@ -5,7 +5,7 @@ import os from 'node:os';
 import path from 'node:path';
 import { execFile } from 'node:child_process';
 const { NODE_ENV } = process.env;
-
+const ytdlpFormat = 'bestvideo[height<=?480]/best';
 
 export default {
 	data: new SlashCommandBuilder()
@@ -63,14 +63,14 @@ export default {
 			return interaction.editReply({ content: '❌ This does not look like a valid url!', ephemeral: true });
 		}
 
-		const aproxFileSize = await utils.getVideoSize(url, 'bestvideo[height<=?480]');
+		const aproxFileSize = await utils.getVideoSize(url, ytdlpFormat);
 		console.log(aproxFileSize);
 
 		if (aproxFileSize > 4) {
 			return interaction.editReply('The file you are trying to convert is too big! Limit is 4 MB');
 		};
 
-		utils.downloadVideo(url, interaction.id, 'bestvideo[height<=?480]')
+		utils.downloadVideo(url, interaction.id, ytdlpFormat)
 			.then(async () => {
 				const file = fs.readdirSync(os.tmpdir()).filter(fn => fn.startsWith(interaction.id));
 				let output = `${os.tmpdir()}/${file}`;
